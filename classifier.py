f0579f84f8ce2e50d11c32988782380e23c4ece2
Purshotam Singh <singhpurshotam48@gmail.com>
2018-03-25 11:53:15 +0530

    second commit

--- classifier.py
+++ classifier.py
@@ -2,22 +2,30 @@ import numpy as np
 import csv
 import pickle
 
-def load_data():
-    with open('trai.txt', encoding="utf8") as f:
-        data=[line for line in f.readlines()]
+def load_train_data():
+    with open ('trai.txt', 'rb') as fp:
+        data_train = pickle.load(fp)
+        return(data_train)
+
+def load_train_target():
+    with open ('dd.txt', 'rb') as fp:
+        data = pickle.load(fp)
         return(data)
 
-def load_target():
-    with open('dd.txt', encoding="utf8") as f:
-        data=[line for line in f.readlines()]
+def load_test_data():
+    with open ('data_train_2.txt', 'rb') as fp:
+        data = pickle.load(fp)
         return(data)
 
-X = load_data()
-y = load_target()
-X_train = X[:-int(0.2*len(X))]
-X_test = X[-int(0.2*len(X)):] 
-y_train = y[:-int(0.2*len(y))]
-y_test = y[-int(0.2*len(y)):] 
+def load_test_target():
+    with open ('target_train_2.txt', 'rb') as fp:
+        data = pickle.load(fp)
+        return(data)
+test_data=load_test_data()
+train_data=load_train_data()
+train_target=load_train_target()
+
+test_target=load_test_target()
 
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -27,8 +35,8 @@ from sklearn.linear_model import SGDClassifier
 dict={'1\n':"Commercial",'2\n':"Maintainence",'3\n':"Safety And Lost and Found",'4\n':"Traffic",'5\n':"Financial",'6\n':"Unclassified"}
 
 text_clf_svm = Pipeline([('vect', CountVectorizer(stop_words='english')),('tfidf', TfidfTransformer()),('clf-svm', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)),])
-_ = text_clf_svm.fit(X_train, y_train)
-predicted_svm = text_clf_svm.predict(X_test)
+_ = text_clf_svm.fit(train_data,train_target)
+predicted_svm = text_clf_svm.predict(test_data)
 #print(predicted_svm)
 #for i in predicted_svm:
     #print(dict[i])
